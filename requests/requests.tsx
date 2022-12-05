1e74209882c5dfbbd8318bba9339675d2211ee70
Oshoke Oyati <oyatioshoke@yahoo.com>
2022-12-05 23:08:39 +0100

    env

--- requests/requests.tsx
+++ requests/requests.tsx
@@ -1,7 +1,7 @@
 // const baseUrl = 'https://api.coingecko.com/api/v3/';
 
 // const lastUrl =
-//   'vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true';
+//   'vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true'
 
 const requests = {
   cryptoList: `${process.env.BASE_URL}coins/markets?${process.env.LAST_URL}`,
